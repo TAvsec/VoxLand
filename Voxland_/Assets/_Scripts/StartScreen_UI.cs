45c1e264e1271eb6fa4430a959c3e1a1628e8477
TAvsec <avsectimotej1@gmail.com>
2017-09-07 21:06:06 +0200

    Added login and register forms
    PHP and MySQL not implemented yet

---
+++ Voxland_/Assets/_Scripts/StartScreen_UI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen_UI : MonoBehaviour {
+
+	public Animator logAnim;
+	public Animator regAnim;
+	
+	public void changeForms(){
+		logAnim.SetTrigger("loginScreen_trigger");
+		regAnim.SetTrigger("registerTrigger");
+	}
+	
+}
